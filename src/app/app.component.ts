8046477a2da2c3cbb6fe4bd64d0c3120074901ee
Cory Rylan <splintercode.cb@gmail.com>
2019-07-24 09:17:28 -0500

    ie: force ie theme updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,14 @@
 import { Component } from '@angular/core';
 
+// This code would ideally be loaded in main.ts before everything else
+// For demonstration it is here with the rest of the theming logic
+import cssVars from 'css-vars-ponyfill';
+import './dropdown';
+cssVars({
+  shadowDOM: true,
+  watch: true
+});
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -13,14 +22,32 @@ export class AppComponent {
   toggleTheme() {
     this.lightTheme = !this.lightTheme;
 
+    // This forces ShadyCSS to update all web components using shadow DOM
     (window as any).ShadyCSS.styleDocument({
       '--root-color': this.lightTheme ? '#2d2d2d' : '#fff',
       '--root-background-color': this.lightTheme ? '#fff' : '#2d2d2d',
       '--dropdown-color': this.lightTheme ? '#2d2d2d' : '#fff',
       '--dropdown-background-color': this.lightTheme ? '#ccc' : '#000',
     });
+
+    // This forces the CSS Vars Ponyfill to update global styles
+    // TODO: need to clean up old style tags previously added
+    const newStyle = document.createElement('style');
+    document.head.appendChild(newStyle);
+    newStyle.id = 'ie-theme';
+    newStyle.innerHTML = `
+      :root {
+        --root-color: ${this.lightTheme ? '#2d2d2d' : '#fff'};
+        --root-background-color: ${this.lightTheme ? '#fff' : '#2d2d2d'};
+        --dropdown-color: ${this.lightTheme ? '#2d2d2d' : '#fff'};
+        --dropdown-background-color: ${this.lightTheme ? '#ccc' : '#000'};
+      }
+    `;
   }
 
+  // This example updates a individual components shadow dom
+  // This works until the element is added or removed and then "forgets" its state
+  // Because of this it is ideal we use a flat global structure for CSS Vars
   toggleTextColor() {
     this.textColor = this.textColor === '#000' ? '#fff' : '#000';
     (window as any).ShadyCSS.styleSubtree(document.querySelector('x-dropdown'), {'--dropdown-color' : this.textColor});
--- src/main.ts
+++ src/main.ts
@@ -4,16 +4,6 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-import cssVars from 'css-vars-ponyfill';
-
-cssVars({
-  shadowDOM: true,
-  watch: true
-});
-
-import './app/my-wc-component';
-import './app/dropdown';
-
 if (environment.production) {
   enableProdMode();
 }
